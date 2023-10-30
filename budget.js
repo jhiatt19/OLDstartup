8158516adbbb332878c3dd6e433188d12d7fd698
hiattj19 <hiattj19@gmail.com>
2023-10-30 16:40:42 -0600

    Budget JS work

--- budget.js
+++ budget.js
@@ -11,14 +11,38 @@ function addTableElement(){
 
 }
 
-const tableBody = document.querySelector('#budgetRows');
-const tableHead = document.querySelector('#budgetHead');
+
 function generateTable() {
     const numRows = prompt("How many rows?");
     const numColumns = prompt("How many columns?");
-    const tableHead = document.createElement("th");
-    const tableRow = document.createElement("tr");
-    tbl.appendChild(row);
+    const tbl = document.createElement('table', 'class="table table-bordered table-hover"');
+    const tableHead = document.createElement('thead', 'class="table-dark"');
+    const tableBody = document.createElement('tbody')
+    const tableRow = document.createElement('tr');
+    const rowData = document.createElement('td');
+    const colHead = document.createElement('th');
+    for (let i = 0; i < numColumns; i++){
+        const colHeadText = document.createTextNode(prompt("Table Head Column"));
+        colHead.append(colHeadText);
+        tableRow.appendChild(colHead);
+    }
+    tableHead.appendChild(tableRow);
+    tbl.appendChild(tableHead);
+    for (let k = 0; k < numRows; k++){
+        for (let j = 0; j < numColumns; j++){
+            if (j == 0){
+                const rowDataText = document.createTextNode(prompt("Row Name?"));
+                rowData = rowDataText;
+                tableRow.appendChild(rowData);
+            }
+            else{
+                const rowDataText = document.createTextNode(prompt(tableHead[k] + tableRow[0] + " = "));
+                rowData = rowDataText;
+                tableRow.appendChild(rowData);
+            }
+        }
+        tableBody.appendChild(tableRow);
+    }
     tbl.appendChild(tableBody);
 }
 
